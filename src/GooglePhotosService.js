a82c026149636f666696672745e61931e047b35e
Michael <mbborromeo@gmail.com>
2019-08-01 11:46:06 +1000

    added loadAlbumDetail in Service and allow for album not found in ViewAlbum

--- src/GooglePhotosService.js
+++ src/GooglePhotosService.js
@@ -1,33 +1,55 @@
+const testData = [
+  {id: '1', title: 'Album 1', description: 'some album description 1'},
+  {id: '2', title: 'Album 2', description: 'some album description 2'},
+  {id: '3', title: 'Album 3', description: 'some album description 3'},
+  {id: '4', title: 'Album 4', description: 'some album description 4'},
+  {id: '5', title: 'Album 5', description: 'some album description 5'},
+]
+
 class GooglePhotosService {
   loadAlbums() {
     return new Promise( function(resolve, reject) {
-      setTimeout( function() {
-        resolve(
-          [
-            { id: 1, title: 'uno'},
-            { id: 2, title: 'oos'},
-            { id: 3, title: 'tres'},
-            { id: 4, title: 'quatro'},
-            { id: 5, title: 'cinqo'},
-          ]
-        )
-      }, 
-      3000);
+      setTimeout( 
+        function() {
+          resolve(
+            /*
+            [
+              { id: 1, title: 'uno'},
+              { id: 2, title: 'oos'},
+              { id: 3, title: 'tres'},
+              { id: 4, title: 'quatro'},
+              { id: 5, title: 'cinqo'},
+            ]
+            */
+            testData.map(function(album) {
+              return {id: album.id, title: album.title}
+            })
+          )
+        }, 
+        3000
+      );
     });
   }
 
   loadAlbumDetail(id) {
     return new Promise( function(resolve, reject) {
-      setTimeout( function() {
-        resolve(       
-          [   
-            { id: 101, description: 'sadsa asd as'}
-          ]
-        )
-      }, 
-      3000);
+      setTimeout( 
+        function() {
+          resolve(       
+            /*[   
+              { id: 101, description: 'sadsa asd as'}
+            ]
+            */
+            testData.find(function(album) {
+              return album.id === id
+            })
+          )
+        }, 
+        3000
+      );
     });
   }
+
 }
 
 export default GooglePhotosService;
--- src/ViewAlbum.js
+++ src/ViewAlbum.js
@@ -7,8 +7,21 @@ function ViewAlbum( props ) {
 
   const service = new GooglePhotosService();
 
-  const [ albumDetails, setAlbumDetails ] = useState( [] ); //undefined
+  const [ albumDetails, setAlbumDetails ] = useState( undefined );
   const [ isLoading, setIsLoading ] = useState( true );
+  
+  //testing
+  /*
+  service.loadAlbumDetail('1')
+  .then(function(result) {
+    console.log('loading 1', result);
+  });
+
+  service.loadAlbumDetail('doesnt-exist')
+  .then(function(result) {
+    console.log('loading doesnt-exist', result);
+  });
+  */
 
   useEffect( 
     function() {
@@ -19,9 +32,10 @@ function ViewAlbum( props ) {
         setIsLoading(false);
       });
     }, 
-    []
+    [props.match] // Q - what is match used for here?
   );
 
+  /*
   const newAlbum = albumDetails.map( function(obj) {
     return <div key={ obj.id }>
       { obj.id } { obj.description }
@@ -30,14 +44,43 @@ function ViewAlbum( props ) {
 
   return <div>
     { newAlbum }
-
-    <br />
-    <Link to="/">Back</Link>
-
+    
     {
       isLoading ? "Loading" : "Not loading"
     }
+
+    <hr />
+    <Link to="/">Back</Link>    
   </div>;
+  */
+
+  /*
+  return (
+    <div>
+      {albumDetails && <h2>{albumDetails.title}</h2>}
+      {albumDetails && <p>{albumDetails.description}</p>}
+      {isLoading && 'Loading...'}
+
+      <hr />
+      <Link to="/">Back to Albums List</Link>
+    </div>
+  )
+  */
+
+  // If the service is finished loading the album, but the album doesnt exist
+  if (!isLoading && !albumDetails) { //isLoaded && !albumDetails
+    return <div>
+      <h2>Album not found</h2>
+    </div>
+  }
+
+  return <div>
+    <h2>Normal display</h2>
+    {isLoading ? <div>Loading...</div> : ''}
+    {albumDetails !== undefined ? <h2>{albumDetails.title}</h2> : ''}
+    {albumDetails !== undefined ? <div>Description: {albumDetails.description}</div> : ''}
+  </div>
+
 }
 
 export default ViewAlbum;
