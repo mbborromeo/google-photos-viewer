ebb1340bfcfee0e1d82d245035de7df7d60f47ec
mbborromeo <mbborromeo@gmail.com>
2020-10-07 01:57:01 -0700

    build - Modal close on click outside image

--- docs/precache-manifest.4017f8bd928dc3099db433f283616240.js
+++ docs/precache-manifest.4017f8bd928dc3099db433f283616240.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "c084af4b586a8362ca6cbf8a513d6b49",
+    "revision": "94bf56cf05eaee9729d6179c3fb5cd66",
     "url": "/google-photos-viewer/index.html"
   },
   {
-    "revision": "04ef7921188003e2d9a8",
-    "url": "/google-photos-viewer/static/css/main.c50c3dee.chunk.css"
+    "revision": "82de2a952530efac0d25",
+    "url": "/google-photos-viewer/static/css/main.8c83082f.chunk.css"
   },
   {
     "revision": "0fa4eae801ad638981cd",
     "url": "/google-photos-viewer/static/js/2.5e87a522.chunk.js"
   },
   {
-    "revision": "04ef7921188003e2d9a8",
-    "url": "/google-photos-viewer/static/js/main.10083cd8.chunk.js"
+    "revision": "82de2a952530efac0d25",
+    "url": "/google-photos-viewer/static/js/main.eae520eb.chunk.js"
   },
   {
     "revision": "67a3c3f1844ba0c70bc4",
